86c63ca44a8f223382b47bc303d687974bea64fc
andreburto <tuglyraisin@gmail.com>
2026-05-11 19:26:04 -0500

    tinkering, 2

--- scratch.c
+++ scratch.c
@@ -4,6 +4,7 @@
 #include <math.h>
 
 int main() {
+    char new_digit = '5';
     double value = 123.456;
     char buffer[64];
 
@@ -17,7 +18,23 @@ int main() {
     printf("Back to float: %g\n", back_to_float);
 
     float add_one = back_to_float + 1.01f;
-    printf("Back to float + 1: %g\n", add_one);
+    printf("Back to float + 1.01: %g\n", add_one);
+
+    len = snprintf(buffer, sizeof(buffer), "%g", add_one);
+    printf("Add one as string: %s\n", buffer);
     
+    // Append new digit to the end of the number
+    // This is a hacky way to do it, but it works for testing
+    if (strchr(buffer, '.')) {
+        // If there's a decimal point, just append the digit
+        strncat(buffer, &new_digit, 1);
+    } else {
+        // If there's no decimal point, add one and then append the digit
+        float new_value = back_to_float * 10 + (new_digit - '0');
+        snprintf(buffer, sizeof(buffer), "%g", new_value);
+    }
+
+    printf("Value after appending digit '%c': %s\n", new_digit, buffer);
+
     return 0;
 }
